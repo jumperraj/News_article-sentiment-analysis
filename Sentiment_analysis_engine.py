6d0131fb6e16061b9b22db29c2d366be9aacadaf
jumperraj <abhir11110@gmail.com>
2022-12-24 11:53:46 +0530

    3rd sub-final commit

--- Sentiment_analysis_engine.py
+++ Sentiment_analysis_engine.py
@@ -5,20 +5,21 @@ from  vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 response=requests.get('https://newsapi.org/v2/top-headlines?country=in&apiKey=4bd9af7ca0814c52a3cfa7a5111fcc92')
 data=response.text
 analyzer= SentimentIntensityAnalyzer()
-print(data)
+# print(data)
 json_data=json.loads(data)
-print(type(json_data["articles"]))
-print(json_data["articles"])
+# print(type(json_data["articles"]))
+# print(json_data["articles"])
 with open('news_article2.json','w',encoding="utf-8") as f:
     f.write(data)
 df1=pd.read_json("news_article2.json")
 pd.set_option('display.max_columns',None)
-print(df1["articles"])
-print(type(df1["articles"][0]))
-print(df1["articles"][0]["title"])
+# print(df1["articles"])
+# print(type(df1["articles"][0]))
+# print(df1["articles"][0]["title"])
 neg=[]
 pos=[]
 neu=[]
+com=[]
 title_l=[]
 for n in range(df1.shape[0]):
     try:
@@ -26,25 +27,29 @@ for n in range(df1.shape[0]):
         desc=df1["articles"][n]["description"]
         title_a= analyzer.polarity_scores(title)
         desc_a= analyzer.polarity_scores(desc)
-        neg.append((title_a["neg"]+desc_a["neg"])+2)
-        pos.append((title_a["pos"]+desc_a["pos"])+2)
-        neu.append((title_a["neu"]+desc_a["neu"])+2)
-        print(title_a)
+        # print(title_a)
+        neg.append((title_a["neg"]+desc_a["neg"])/2)
+        pos.append((title_a["pos"]+desc_a["pos"])/2)
+        neu.append((title_a["neu"]+desc_a["neu"])/2)
+        com.append((title_a["compound"]+desc_a["compound"])/2)
+        # print(title_a)
     except:
         neg.append(0.0)
         pos.append(0.0)
         neu.append(0.0)
-
+        com.append(0.0)
     title_l.append(title)
 df2=pd.DataFrame()
 df2["Headline"]=title_l
 df2["Negative"]=neg
 df2["Positive"]=pos
 df2["Neutral"]=neu
-
+df2["Compound"]=com
 pd.set_option('display.max_columns',None)
-
-print("Average nagative sentiment",df2.mean()[0])
-print("Average positive sentiment",df2.mean()[1])
-print("Average neutral sentiment",df2.mean()[2])
-
+print(df2.head())
+string=f" Average nagative sentiment :   {df2.mean(numeric_only=True)[0]} \n Average positive sentiment :   {df2.mean(numeric_only=True)[1]} \n Average neutral sentiment :   {df2.mean(numeric_only=True)[2]} \n Average compound sentiment :   {df2.mean(numeric_only=True)[3]} \n"
+# print("Average nagative sentiment :",df2.mean(numeric_only=True)[0])
+# print("Average positive sentiment :",df2.mean(numeric_only=True)[1])
+# print("Average neutral sentiment :",df2.mean(numeric_only=True)[2])
+# print(" Average compound sentiment :",df2.mean(numeric_only=True)[3])
+print(string)
---
+++ Table_view.py
@@ -0,0 +1,46 @@
+from tkinter import *
+from tkinter.ttk import Style
+
+from pandastable import Table, TableModel
+import Sentiment_analysis_engine
+
+class TestApp(Frame):
+        """Basic test frame for the table"""
+        def __init__(self, parent=None):
+            self.parent = parent
+            Frame.__init__(self)
+            self.main = self.master
+            self.master.geometry('1500x700')
+            self.main.title('Table app')
+            self.configure(bg="black")
+            f = Frame(self.main)
+            f.place(x=0,y=0)
+
+            f.pack(fill=BOTH,expand=1,padx=30,pady=30)
+            # df2 = TableModel.getSampleData()
+            self.table = pt = Table(f, dataframe=Sentiment_analysis_engine.df2,
+                                    showtoolbar=True, showstatusbar=True)
+            self.initUI()
+            pt.show()
+
+        def initUI(self):
+            self.style = Style()
+            self.pack(fill=BOTH, expand=1)
+            Button1 = Button(self, text="Generate Sentiment",
+                                command=self.display,fg="black",bg="#d7f542",width=25,height=2,border=0,font="lucida 15 bold")
+            Button1.place(x=600, y=0)
+        def display(self):
+            global screen
+            screen = Toplevel(self.main)
+            screen.title("Login")
+            screen.geometry("1300x120")
+            Label(screen, text=f"{Sentiment_analysis_engine.string}",fg="white",bg="black",borderwidth=2, font="time 15 ").pack(fill=BOTH)
+
+
+
+
+
+
+app = TestApp()
+#launch the app
+app.mainloop()
